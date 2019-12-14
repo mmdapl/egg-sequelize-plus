1cab3556046ebe75a6247b19994726bfef1ba604
Taylor <2237221210@qq.com>
2019-12-14 11:19:24 +0800

    修改插件打印信息格式

--- agent.js
+++ agent.js
@@ -10,7 +10,7 @@
 const mysql = require('mysql2');
 module.exports = agent => {
   const { host, port, username, password, database } = agent.config.sequelizePlus;
-  const sql = 'CREATE DATABASE IF NOT EXISTS ' + database + ' default charset utf8 COLLATE utf8_general_ci';
+  const sql = '[egg-sequelize-pluse] CREATE DATABASE IF NOT EXISTS ' + database + ' default charset utf8 COLLATE utf8_general_ci';
   // build a connection with database
   const connection = mysql.createConnection({ host, port, user: username, password });
   // open connection
@@ -21,8 +21,8 @@ module.exports = agent => {
       // create failed
       agent.logger.error('DataBase create failed ，check sequelize-plus config carefully ', err);
     }
-    agent.logger.info('DataBase create success , the name is ' + database);
-    agent.logger.info('egg-sequelize-pluse build conection successed ,please begin use it to operate DataBase ');
+    agent.logger.info('[egg-sequelize-pluse] DataBase create success , the name is ' + database);
+    agent.logger.info('[egg-sequelize-pluse] build conection successed ,please begin use it to operate DataBase ');
     // close connection with database
     connection.end();
   });
