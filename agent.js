1c3afa9d468880ec7c152e366310593a8caa56e5
Taylor <2237221210@qq.com>
2019-12-14 11:40:39 +0800

    修改创建数据库sql语句

--- agent.js
+++ agent.js
@@ -10,7 +10,7 @@
 const mysql = require('mysql2');
 module.exports = agent => {
   const { host, port, username, password, database } = agent.config.sequelizePlus;
-  const sql = '[egg-sequelize-pluse] CREATE DATABASE IF NOT EXISTS ' + database + ' default charset utf8 COLLATE utf8_general_ci';
+  const sql = 'CREATE DATABASE IF NOT EXISTS ' + database + ' default charset utf8 COLLATE utf8_general_ci';
   // build a connection with database
   const connection = mysql.createConnection({ host, port, user: username, password });
   // open connection
@@ -19,10 +19,10 @@ module.exports = agent => {
   connection.query(sql, err => {
     if (err) {
       // create failed
-      agent.logger.error('DataBase create failed ，check sequelize-plus config carefully ', err);
+      agent.logger.error('[egg-sequelize-plus] DataBase create failed ，check egg-sequelize-plus config carefully ', err);
     }
-    agent.logger.info('[egg-sequelize-pluse] DataBase create success , the name is ' + database);
-    agent.logger.info('[egg-sequelize-pluse] build conection successed ,please begin use it to operate DataBase ');
+    agent.logger.info('[egg-sequelize-plus] DataBase create success , the name is ' + database);
+    agent.logger.info('[egg-sequelize-plus] build conection successed ,please begin use it to operate DataBase ');
     // close connection with database
     connection.end();
   });
